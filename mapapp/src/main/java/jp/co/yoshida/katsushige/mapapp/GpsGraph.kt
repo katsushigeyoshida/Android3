7e94d505204a349c7b827f4e752e8acac9b99293
Katsushige Yoshida <k-yosida@g06.itscom.net>
2023-03-02 11:20:01 +0900

    拡大縮小移動機能の追加追加とデータ種別の変更

--- mapapp/src/main/java/jp/co/yoshida/katsushige/mapapp/GpsGraph.kt
+++ mapapp/src/main/java/jp/co/yoshida/katsushige/mapapp/GpsGraph.kt
@@ -26,17 +26,14 @@ class GpsGraph : AppCompatActivity() {
     lateinit var spVertical: Spinner
     lateinit var spHorizontal: Spinner
     lateinit var spAverageCount: Spinner
+    lateinit var btZoomUp: Button
+    lateinit var btZoomDown: Button
+    lateinit var btMoveLeft: Button
+    lateinit var btMoveRight: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gps_graph)
-
-        binding = ActivityGpsGraphBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        linearLayoutGraphview = binding.linearLayoutGraphview
-        spVertical = binding.spVertical
-        spHorizontal = binding.spHorizontal
-        spAverageCount = binding.spAverageCount
 
         val filePath = intent.getStringExtra("FILE")
         val gpsTitle = intent.getStringExtra("TITLE")
@@ -63,12 +60,15 @@ class GpsGraph : AppCompatActivity() {
             Toast.makeText(this, "データがありません", Toast.LENGTH_LONG).show()
             return
         }
+
         gpxReader.setGpsInfoData()
         mGpsGraphView = GpsGraphView(this, gpxReader)
-        linearLayoutGraphview.addView(mGpsGraphView)
 
+        //  初期化
+        initControl()
+
+        //  API30以上でのScreenSizek取得
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            //  API30以上でのScreenSizek取得
             val windowMetrics = this.windowManager.currentWindowMetrics
             val insets: Insets = windowMetrics.windowInsets
                 .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
@@ -79,16 +79,26 @@ class GpsGraph : AppCompatActivity() {
             Log.d(TAG,"onCreate: ScreenSize "+ScreenWidth+" "+ScreenHeight+" "+StatusBar+" "+NavigationBar)
             mGpsGraphView.setInitGraphScreen(ScreenWidth, ScreenHeight - 600)    //  600はGraphエリア以外
         }
-
-        //  初期化
-        init()
     }
 
     //  初期化処理
-    fun init() {
+    fun initControl() {
+        binding = ActivityGpsGraphBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        linearLayoutGraphview = binding.linearLayoutGraphview
+        spVertical = binding.spVertical
+        spHorizontal = binding.spHorizontal
+        spAverageCount = binding.spAverageCount
+        btZoomUp = binding.button29
+        btZoomDown = binding.button30
+        btMoveLeft = binding.button31
+        btMoveRight = binding.button32
+
+        linearLayoutGraphview.addView(mGpsGraphView)
+
         //  縦軸のデータ種別の設定
-        var verticalTypeAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, mGpsGraphView.mGraphYType)
-        spVertical.adapter = verticalTypeAdapter
+        spVertical.adapter = ArrayAdapter(this,
+            R.layout.support_simple_spinner_dropdown_item, mGpsGraphView.mGraphYType)
         spVertical.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 mGpsGraphView.mYTypeTitle = mGpsGraphView.mGraphYType[position]
@@ -100,8 +110,8 @@ class GpsGraph : AppCompatActivity() {
         }
 
         //  横軸のデータ種別の設定
-        var horizontalTypeAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, mGpsGraphView.mGraphXType)
-        spHorizontal.adapter = horizontalTypeAdapter
+        spHorizontal.adapter = ArrayAdapter(this,
+            R.layout.support_simple_spinner_dropdown_item, mGpsGraphView.mGraphXType)
         spHorizontal.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 mGpsGraphView.mXTypeTitle = mGpsGraphView.mGraphXType[position]
@@ -113,8 +123,8 @@ class GpsGraph : AppCompatActivity() {
         }
 
         //  移動平均のデータ数の設定
-        var averageCountTypeAdapter = ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, mGpsGraphView.mAverageCount)
-        spAverageCount.adapter = averageCountTypeAdapter
+        spAverageCount.adapter = ArrayAdapter(this,
+            R.layout.support_simple_spinner_dropdown_item, mGpsGraphView.mAverageCount)
         spAverageCount.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 mGpsGraphView.mAverageCountTitle = mGpsGraphView.mAverageCount[position]
@@ -123,6 +133,38 @@ class GpsGraph : AppCompatActivity() {
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 TODO("Not yet implemented")
             }
+        }
+
+        /**
+         * [⊕]ボタン 横方向拡大
+         */
+        btZoomUp.setOnClickListener {
+            mGpsGraphView.zoomDisp(2.0)
+            mGpsGraphView.reDraw()
+        }
+
+        /**
+         * [⊖]ボタン 横方向縮小
+         */
+        btZoomDown.setOnClickListener {
+            mGpsGraphView.zoomDisp(0.5)
+            mGpsGraphView.reDraw()
+        }
+
+        /**
+         * グラフを左に移動
+         */
+        btMoveLeft.setOnClickListener {
+            mGpsGraphView.moveDisp(-0.5)
+            mGpsGraphView.reDraw()
+        }
+
+        /**
+         * グラフを右に移動
+         */
+        btMoveRight.setOnClickListener {
+            mGpsGraphView.moveDisp(0.5)
+            mGpsGraphView.reDraw()
         }
     }
 
